2d168fb71aad14f8f10ae187c496d48e1d792985
paruthidotexe <paruthidotexe@gmail.com>
2015-04-19 19:44:20 +0530

    Bug fix light map Story Screen ready

---
+++ Assets/Znop/Scripts/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+		if(Input.GetKeyUp(KeyCode.S))
+		{
+			Application.LoadLevel("Story");
+		}
+	
+	}
+}
---
+++ Assets/Znop/Scripts/StoryScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class StoryScript : MonoBehaviour {
+
+	public  List<GameObject> Pages;
+
+	int curPage = 0;
+	float doubleClickTimer = 0.5f;
+	bool clickBlocked = false;
+
+	void Start () {
+		UpdateUI();	
+	}
+
+	void Update () {
+		if(clickBlocked)
+		{
+			doubleClickTimer -= Time.deltaTime;
+			if(doubleClickTimer < 0)
+			{
+				clickBlocked = false;
+				doubleClickTimer = 0.5f;
+			}
+		}
+	
+	}
+
+	void UpdateUI()
+	{
+		for(int i = 0; i < Pages.Count; i++)
+		{
+			Pages[i].SetActive(false);
+		}
+		Pages[curPage].SetActive(true);
+	}
+
+	public void OnNextPage()
+	{
+		if(clickBlocked)
+		{
+
+		}
+		else
+		{
+			if(curPage + 1 >= Pages.Count)
+			{
+				Application.LoadLevel("MainMenu");
+			}
+			else
+			{
+				curPage++;
+				UpdateUI();
+			}
+		}
+	}
+
+}
